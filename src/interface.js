bbcac07835df7d7f12e2a7705b1b402fbe0a741d
olga <itsglimerry@gmail.com>
2021-03-10 12:33:34 +0000

    Weather search draft added.

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#thermostat-temperature').text(thermostat.temperature);
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=85517936ebe5c7470d0c186d231bd426', function(data) {
+    console.log(data);
+    $('#current-temperature').text(data.main.temp);
+  })
+
+  $('#select-city-submit').click(function(event) {
+    event.preventDefault();
+    var city = $('#select-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=85517936ebe5c7470d0c186d231bd426&units=metric', function(data) {
+     console.log(data);
+    $('#current-temperature').text(data.main.temp);
+  })
+})
+
   $('#temperature-up').click(function() {
     thermostat.up()
     updateTemperature()
